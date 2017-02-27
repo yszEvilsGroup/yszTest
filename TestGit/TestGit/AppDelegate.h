eb994c16b2a4eda24e36306b859af2d7a2968010
Lee <347456222@qq.com>
2017-02-27 11:55:54 +0800

    Test

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Lee on 2017/2/27.
+//  Copyright © 2017年 李家乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by Lee on 2017/2/27.
+//  Copyright © 2017年 李家乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
